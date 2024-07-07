962e77a52d9c2a1ac9ef2b0d80953c240f8f49f1
Babalola-Yusuf <yusufbabalola01@gmail.com>
2024-07-07 13:07:01 +0100

    completed the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <header className="bg-black-normal text-white p-4 flex justify-between items-center">
+  <header className="bg-white text-black-normal font-bold p-4 flex justify-between items-center">
     <div className="logo">
-      <Link to="/">Company Logo</Link>
+      <Link to="/"><img src="../images/logo.png" alt="logo" /></Link>
     </div>
-    <nav>
+    
+    <nav >
       <ul className="flex space-x-4">
-        <li><Link to="/products">Products</Link></li>
-        <li><Link to="/cart">Cart</Link></li>
+        <li><Link to="/">Women Skincare</Link></li>
+        <li><Link to="/">Men Skincare</Link></li>
+        <li><Link to="/">Blog</Link></li>
+        <li><Link to="/">Contact Us</Link></li>
+        <li><Link to="/">Our Brand</Link></li>
       </ul>
     </nav>
+
+    <ul className="flex space-x-4 items-center">
+        <li><Link to="/cart"></Link><img src="../icons/search.svg" alt="" /></li>
+        <li><img src="../icons/Line 1.svg" alt="line1" /></li>
+        <div className="flex space-x-1 items-center">
+          <li><Link to="/login"><img src="../icons/Ellipse 17.svg" alt="user" /></Link></li>
+          <li><Link to="/cart"><p>DesignKid</p></Link></li>
+          <li><Link to="/cart"><img src="../icons/arrow_left.svg" alt="arrow left" /></Link></li>
+        </div>
+        
+        <li><Link to="/cart"><img src="../icons/shopping_cart.svg" alt="shopping cart" /></Link></li>
+        <div className="flex space-x-1 items-center ">
+          <li><Link to="/cart"><img src="../icons/language.svg" alt="language" /></Link></li>
+          <li><Link to="/cart"><p> NGN (EN)</p></Link></li>
+        </div>
+        
+
+    </ul>
   </header>
 );
 
